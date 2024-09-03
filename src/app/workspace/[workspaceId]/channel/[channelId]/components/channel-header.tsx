052b640a8d386fa53c17b8bf1b366d6c253c8613
web3crafter <web3crafter@proton.me>
2024-09-03 17:14:03 +0200

    add validation for admin functionality

--- src/app/workspace/[workspaceId]/channel/[channelId]/components/channel-header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/components/channel-header.tsx
@@ -1,13 +1,17 @@
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "sonner";
 import { FaChevronDown } from "react-icons/fa";
 import { TrashIcon } from "lucide-react";
-import { toast } from "sonner";
 
 import { replaceWhiteSpace } from "@/lib/utils";
 import { useChannelId } from "@/hooks/use-channel-id";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 import { useUpdateChannel } from "@/features/channels/api/use-update-channel";
 import { useRemoveChannel } from "@/features/channels/api/use-remove-channel";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -21,25 +25,34 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { useConfirm } from "@/hooks/use-confirm";
 
 interface HeaderProps {
   title: string;
 }
 
 export const ChannelHeader = ({ title }: HeaderProps) => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
   const channelId = useChannelId();
   const [isEditOpen, setIsEditOpen] = useState(false);
   const [name, setName] = useState(title);
   const [ConfirmRemoveDialog, confirmRemoveChannel] = useConfirm(
-    "Are you sure you want to delete this channel?",
-    "This action cannot be undone.",
+    "Delete this channel?",
+    "Are you sure you want to delete this channel? This action cannot be undone.",
   );
 
+  const { data: member } = useCurrentMember({
+    workspaceId,
+  });
   const { mutate: updateChannel, isPending: isUpdatingChannel } =
     useUpdateChannel();
   const { mutate: removeChannel, isPending: isRemovingChannel } =
     useRemoveChannel();
+
+  const handleEditOpen = (value: boolean) => {
+    if (member?.role !== "admin") return;
+    setIsEditOpen(value);
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = replaceWhiteSpace(e.target.value);
@@ -64,6 +77,7 @@ export const ChannelHeader = ({ title }: HeaderProps) => {
   };
 
   const handleRemoveChannel = async () => {
+    if (member?.role !== "admin") return;
     const ok = await confirmRemoveChannel();
 
     if (!ok) return;
@@ -73,6 +87,7 @@ export const ChannelHeader = ({ title }: HeaderProps) => {
       {
         onSuccess: () => {
           toast.success("Channel deleted");
+          router.push(`/workspace/${workspaceId}`);
         },
         onError: () => {
           toast.error("Failed to delete channel, Please try again");
@@ -82,75 +97,73 @@ export const ChannelHeader = ({ title }: HeaderProps) => {
   };
 
   return (
-    <>
+    <div className="flex h-[49px] items-center overflow-hidden border-b px-4">
       <ConfirmRemoveDialog />
-      <div className="flex h-[49px] items-center overflow-hidden border-b px-4">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button
-              variant={"ghost"}
-              className="w-auto overflow-hidden px-2 text-lg font-semibold"
-              size={"sm"}
-            >
-              <span className="truncate"># {title}</span>
-              <FaChevronDown className="ml-2 size-2.5" />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="overflow-hidden p-0">
-            <DialogHeader className="border-b bg-secondary/5 p-4">
-              <DialogTitle># {title}</DialogTitle>
-              <DialogDescription className="sr-only">
-                Rename or delete channel
-              </DialogDescription>
-            </DialogHeader>
-            <div className="flex flex-col gap-y-2 px-4 pb-4">
-              <Dialog open={isEditOpen} onOpenChange={setIsEditOpen}>
-                <DialogTrigger asChild>
-                  <div className="cursor-pointer rounded-lg border px-5 py-4 hover:bg-secondary/5">
-                    <div className="flex items-center justify-between">
-                      <p className="text-sm font-semibold">Channel name</p>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant={"ghost"}
+            className="w-auto overflow-hidden px-2 text-lg font-semibold"
+            size={"sm"}
+          >
+            <span className="truncate"># {title}</span>
+            <FaChevronDown className="ml-2 size-2.5" />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="overflow-hidden p-0">
+          <DialogHeader className="border-b bg-secondary/5 p-4">
+            <DialogTitle># {title}</DialogTitle>
+            <DialogDescription className="sr-only">
+              Rename or delete channel
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-2 px-4 pb-4">
+            <Dialog open={isEditOpen} onOpenChange={handleEditOpen}>
+              <DialogTrigger asChild>
+                <div className="cursor-pointer rounded-lg border px-5 py-4 hover:bg-secondary/5">
+                  <div className="flex items-center justify-between">
+                    <p className="text-sm font-semibold">Channel name</p>
+                    {member?.role === "admin" && (
                       <p className="text-sm font-semibold text-[#1264a3] hover:underline">
                         Edit
                       </p>
-                    </div>
-                    <p className="text-sm"># {title}</p>
+                    )}
                   </div>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>Rename this channel</DialogTitle>
-                    <DialogDescription className="sr-only">
-                      Rename channel and click save when done
-                    </DialogDescription>
-                  </DialogHeader>
-                  <form onSubmit={handleEditWorkspace} className="space-y-4">
-                    <Input
-                      value={name}
-                      onChange={handleChange}
-                      disabled={isUpdatingChannel}
-                      required
-                      autoFocus
-                      minLength={3}
-                      maxLength={80}
-                      placeholder="e.g. plan-budget"
-                    />
-                    <DialogFooter>
-                      <DialogClose asChild>
-                        <Button
-                          variant={"outline"}
-                          disabled={isUpdatingChannel}
-                        >
-                          Cancel
-                        </Button>
-                      </DialogClose>
-                      <Button type="submit" disabled={isUpdatingChannel}>
-                        Save
+                  <p className="text-sm"># {title}</p>
+                </div>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Rename this channel</DialogTitle>
+                  <DialogDescription className="sr-only">
+                    Rename channel and click save when done
+                  </DialogDescription>
+                </DialogHeader>
+                <form onSubmit={handleEditWorkspace} className="space-y-4">
+                  <Input
+                    value={name}
+                    onChange={handleChange}
+                    disabled={isUpdatingChannel}
+                    required
+                    autoFocus
+                    minLength={3}
+                    maxLength={80}
+                    placeholder="e.g. plan-budget"
+                  />
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variant={"outline"} disabled={isUpdatingChannel}>
+                        Cancel
                       </Button>
-                    </DialogFooter>
-                  </form>
-                </DialogContent>
-              </Dialog>
-
+                    </DialogClose>
+                    <Button type="submit" disabled={isUpdatingChannel}>
+                      Save
+                    </Button>
+                  </DialogFooter>
+                </form>
+              </DialogContent>
+            </Dialog>
+            {member?.role === "admin" && (
               <button
                 onClick={handleRemoveChannel}
                 className="flex cursor-pointer items-center gap-x-2 rounded-lg border px-5 py-4 text-rose-600 hover:bg-secondary/5"
@@ -159,10 +172,10 @@ export const ChannelHeader = ({ title }: HeaderProps) => {
                 <TrashIcon className="size-4" />
                 <p className="text-sm font-semibold">Delete channel</p>
               </button>
-            </div>
-          </DialogContent>
-        </Dialog>
-      </div>
-    </>
+            )}
+          </div>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };

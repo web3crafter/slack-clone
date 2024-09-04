e73fa3be6bd6ecbb42c0b1327db4689accb797ca
web3crafter <web3crafter@proton.me>
2024-09-04 14:07:09 +0200

    add '@emoji-mart' and functionality to pick emoji

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -7,16 +7,18 @@ import {
 } from "react";
 import Quill, { QuillOptions } from "quill";
 import { Delta, Op } from "quill/core";
+import { ImageIcon, Smile, XIcon } from "lucide-react";
 import { PiTextAa } from "react-icons/pi";
 import { MdSend } from "react-icons/md";
-import { ImageIcon, Smile } from "lucide-react";
 
 import "quill/dist/quill.snow.css";
 
+import { cn } from "@/lib/utils";
+
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/hint";
-import { cn } from "@/lib/utils";
-import { current } from "../../convex/members";
+import { EmojiPopover } from "@/components/emoji-popover";
+import Image from "next/image";
 
 type EditorValue = {
   image: File | null;
@@ -42,6 +44,7 @@ const Editor = ({
   placeholder = "Write something...",
 }: EditorProps) => {
   const [text, setText] = useState("");
+  const [image, setImage] = useState<File | null>(null);
   const [isToolbarVisible, setIsToolbarVisible] = useState(true);
 
   const submitRef = useRef(onSubmit);
@@ -50,6 +53,7 @@ const Editor = ({
   const defaultValueRef = useRef(defaultValue);
   const containerRef = useRef<HTMLDivElement>(null);
   const disabledRef = useRef(disabled);
+  const imageElementRef = useRef<HTMLInputElement>(null);
 
   useLayoutEffect(() => {
     submitRef.current = onSubmit;
@@ -134,12 +138,47 @@ const Editor = ({
     }
   };
 
+  const onEmojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+    quill?.insertText(quill.getSelection()?.index || 0, emoji.native);
+  };
+
   const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
+      <input
+        type="file"
+        accept="image/*"
+        ref={imageElementRef}
+        onChange={(e) => setImage(e.target.files![0])}
+        className="hidden"
+      />
       <div className="flex flex-col overflow-hidden rounded-md border border-slate-200 transition focus-within:border-slate-300 focus-within:shadow-sm dark:border-secondary/50 focus-within:dark:border-secondary">
         <div ref={containerRef} className="ql-custom h-full" />
+        {!!image && (
+          <div className="p-2">
+            <div className="group/image relative flex size-[62px] items-center justify-center">
+              <Hint label="Remove image">
+                <button
+                  onClick={() => {
+                    setImage(null);
+                    imageElementRef.current!.value = "";
+                  }}
+                  className="absolute -right-2.5 -top-2.5 z-[4] hidden size-6 items-center justify-center rounded-full border-2 border-white bg-black/70 text-white hover:bg-black group-hover/image:flex"
+                >
+                  <XIcon className="size-3.5" />
+                </button>
+              </Hint>
+              <Image
+                src={URL.createObjectURL(image)}
+                alt="uploaded image"
+                fill
+                className="overflow-hidden rounded-xl border object-cover"
+              />
+            </div>
+          </div>
+        )}
         <div className="z-[5] flex px-2 pb-2">
           <Hint
             label={isToolbarVisible ? "Hide formatting" : "Show formatting"}
@@ -153,23 +192,18 @@ const Editor = ({
               <PiTextAa className="size-4" />
             </Button>
           </Hint>
-          <Hint label="Emoji">
-            <Button
-              variant={"ghost"}
-              size={"iconSm"}
-              disabled={disabled}
-              onClick={() => {}}
-            >
+          <EmojiPopover onEmojiSelect={onEmojiSelect}>
+            <Button variant={"ghost"} size={"iconSm"} disabled={disabled}>
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopover>
           {variant === "create" && (
             <Hint label="Image">
               <Button
                 variant={"ghost"}
                 size={"iconSm"}
                 disabled={disabled}
-                onClick={() => {}}
+                onClick={() => imageElementRef.current?.click()}
               >
                 <ImageIcon className="size-4" />
               </Button>
@@ -212,11 +246,18 @@ const Editor = ({
           )}
         </div>
       </div>
-      <div className="flex justify-end p-2 text-[10px] text-muted-foreground">
-        <p>
-          <strong>Shift + Return</strong> to add a new line
-        </p>
-      </div>
+      {variant === "create" && (
+        <div
+          className={cn(
+            "flex justify-end p-2 text-[10px] text-muted-foreground opacity-0 transition",
+            !isEmpty && "opacity-100",
+          )}
+        >
+          <p>
+            <strong>Shift + Return</strong> to add a new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/components/emoji-popover.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+interface EmojiPopoverProps {
+  children: React.ReactNode;
+  hint?: string;
+  onEmojiSelect: (emoji: any) => void;
+}
+
+export const EmojiPopover = ({
+  children,
+  onEmojiSelect,
+  hint = "Emoji",
+}: EmojiPopoverProps) => {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  const onSelect = (emoji: any) => {
+    onEmojiSelect(emoji);
+    setPopoverOpen(false);
+
+    setTimeout(() => {
+      setTooltipOpen(false);
+    }, 500);
+  };
+
+  return (
+    <TooltipProvider>
+      <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
+        <Tooltip
+          open={tooltipOpen}
+          onOpenChange={setTooltipOpen}
+          delayDuration={50}
+        >
+          <PopoverTrigger asChild>
+            <TooltipTrigger asChild>{children}</TooltipTrigger>
+          </PopoverTrigger>
+          <TooltipContent className="border-white/5 bg-black text-white">
+            <p className="text-xs font-medium">{hint}</p>
+          </TooltipContent>
+        </Tooltip>
+        <PopoverContent className="shadow.none w-full border-none p-0">
+          <Picker data={data} onEmojiSelect={onSelect} />
+        </PopoverContent>
+      </Popover>
+    </TooltipProvider>
+  );
+};

9e6dc00dd6ef616d613a85e240d513d31acbf7a4
web3crafter <web3crafter@proton.me>
2024-09-04 14:26:49 +0200

    add frontend onSubmit, updated schema to include messages table

--- src/app/workspace/[workspaceId]/channel/[channelId]/components/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/components/chat-input.tsx
@@ -11,12 +11,22 @@ interface ChatInputProps {
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const editorRef = useRef<Quill | null>(null);
 
+  const handleSubmit = ({
+    body,
+    image,
+  }: {
+    body: string;
+    image: File | null;
+  }) => {
+    console.log({ body, image });
+  };
+
   return (
     <div className="w-full px-5">
       <Editor
         variant="create"
         placeholder={placeholder}
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         disabled={false}
         innerRef={editorRef}
       />
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -85,8 +85,22 @@ const Editor = ({
             enter: {
               key: "Enter",
               handler: () => {
-                //TODO: submit form
-                return;
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                if (isEmpty) {
+                  return;
+                }
+
+                const body = JSON.stringify(quill.getContents());
+                submitRef.current?.({
+                  body,
+                  image: addedImage,
+                });
               },
             },
             shift_enter: {
@@ -143,7 +157,7 @@ const Editor = ({
     quill?.insertText(quill.getSelection()?.index || 0, emoji.native);
   };
 
-  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
@@ -214,14 +228,19 @@ const Editor = ({
               <Button
                 variant={"outline"}
                 size={"sm"}
-                onClick={() => {}}
-                disabled={false}
+                onClick={onCancel}
+                disabled={disabled}
               >
                 Cancel
               </Button>
               <Button
                 size={"sm"}
-                onClick={() => {}}
+                onClick={() => {
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  });
+                }}
                 disabled={disabled || isEmpty}
                 className="bg-[#007a5a] text-white hover:bg-[#007a5a]/80"
               >
@@ -233,7 +252,12 @@ const Editor = ({
             <Button
               size={"iconSm"}
               disabled={disabled || isEmpty}
-              onClick={() => {}}
+              onClick={() => {
+                onSubmit({
+                  body: JSON.stringify(quillRef.current?.getContents()),
+                  image,
+                });
+              }}
               className={cn(
                 "ml-auto",
                 isEmpty
